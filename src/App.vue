740c0fe68d6c913f8a5fcd8e936f106aa322ede2
bibenga <{ID}+{username}@users.noreply.github.com>
2023-06-29 09:03:46 +0000

    add code for redirect to login page when user pressed a logout button in another tab

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,24 @@
   <router-view />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Auth, onAuthStateChanged } from 'firebase/auth';
+import { inject } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+const fireauth = inject<Auth>('fireauth');
+onAuthStateChanged(fireauth, (authUser) => {
+  // we should redirect to login page when user pressed a logout button in another tab
+  console.log('[app]', route.path, authUser);
+  if (authUser === null && route.meta.requiresAuth) {
+    router.push({
+      path: '/login',
+      query: {
+        redirect: route.fullPath,
+      },
+    });
+  }
+});
+</script>
