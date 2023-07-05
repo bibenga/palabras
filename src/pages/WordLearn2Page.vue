1c8dfa976e94eb7cbc7d58b3583149aff41f69d0
bibenga <{ID}+{username}@users.noreply.github.com>
2023-07-05 08:30:48 +0000

    adds animation

--- src/pages/WordLearn2Page.vue
+++ src/pages/WordLearn2Page.vue
@@ -12,25 +12,27 @@
       <q-separator />
 
       <q-card-section>
-        <div v-for="(card, index) in cards" :key="index" class="row">
+        <div v-for="(item, index) in rows" :key="index" class="row">
           <div class="col-6 q-pa-xs">
             <q-btn
+              :class="{ shake: incorrect && item.word1Id == word1Selected }"
               style="width: 100%; font-size: 1.2em"
-              :outline="getOutline(card.word1Id, word1Selected)"
-              :disable="getDisabled(card.word1Id, word1Selected)"
-              :color="getColor(card.word1Id, word1Selected)"
-              :label="card.word1"
-              @click="() => word1Clicked(card)"
+              :outline="getOutline(item.word1Id, word1Selected)"
+              :disable="getDisabled(item.word1Id, word1Selected)"
+              :color="getColor(item.word1Id, word1Selected)"
+              :label="item.word1"
+              @click="() => word1Clicked(item)"
             />
           </div>
           <div class="col-6 q-pa-xs">
             <q-btn
               style="width: 100%; font-size: 1.2em"
-              :outline="getOutline(card.word2Id, word2Selected)"
-              :disable="getDisabled(card.word2Id, word2Selected)"
-              :color="getColor(card.word2Id, word2Selected)"
-              :label="card.word2"
-              @click="() => word2Clicked(card)"
+              :class="{ shake: incorrect && item.word2Id == word2Selected }"
+              :outline="getOutline(item.word2Id, word2Selected)"
+              :disable="getDisabled(item.word2Id, word2Selected)"
+              :color="getColor(item.word2Id, word2Selected)"
+              :label="item.word2"
+              @click="() => word2Clicked(item)"
             />
           </div>
         </div>
@@ -72,7 +74,7 @@
 
       <q-card-actions>
         <q-btn
-          v-if="cards.length == correct.length"
+          v-if="rows.length == correct.length"
           @click="() => init()"
           label="Siguiente"
           unelevated
@@ -106,6 +108,34 @@
   margin-top: 8px;
   margin-left: 0px !important;
 }
+
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 </style>
 
 <script setup lang="ts">
@@ -119,7 +149,7 @@ const $q = useQuasar();
 const wordsStore = useWordsStore();
 const { ready } = storeToRefs(wordsStore);
 
-const cards = ref<object[]>([]);
+const rows = ref<object[]>([]);
 const correct = ref<string[]>([]);
 const incorrect = ref(false);
 const word1Selected = ref('');
@@ -172,7 +202,7 @@ const init = () => {
       }
     }
   }
-  cards.value = res;
+  rows.value = res;
   correct.value = [];
   incorrect.value = false;
   word1Selected.value = '';
@@ -207,7 +237,7 @@ const getOutline = (wordId: string, selectedWordId: string): boolean => {
 };
 
 let errorTimer = null;
-const validate = () => {
+const validate = (): void => {
   if (word1Selected.value === '' || word2Selected.value === '') {
     return;
   }
@@ -222,7 +252,7 @@ const validate = () => {
       word2Selected.value = '';
       incorrect.value = false;
       errorTimer = null;
-    }, 1500);
+    }, 900);
   }
 };
 onUnmounted(() => {
@@ -232,20 +262,20 @@ onUnmounted(() => {
   }
 });
 
-const word1Clicked = (card) => {
-  if (word1Selected.value == card.word1Id) {
+const word1Clicked = (item): void => {
+  if (word1Selected.value == item.word1Id) {
     word1Selected.value = '';
   } else {
-    word1Selected.value = card.word1Id;
+    word1Selected.value = item.word1Id;
   }
   validate();
 };
 
-const word2Clicked = (card) => {
-  if (word2Selected.value == card.word2Id) {
+const word2Clicked = (item): void => {
+  if (word2Selected.value == item.word2Id) {
     word2Selected.value = '';
   } else {
-    word2Selected.value = card.word2Id;
+    word2Selected.value = item.word2Id;
   }
   validate();
 };
